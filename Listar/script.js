d61d8cdd7673c6f78556ae741fb41be5e18874a5
Isaque Estolano de Souza <isaqueestolanossi04@gmail.com>
2024-10-17 23:14:54 -0300

    Andamento...

--- Listar/script.js
+++ Listar/script.js
@@ -1,22 +1,37 @@
 var itens = []
 c=0
+
 function add(){
     //Aumentando!
     const minhaDiv = document.getElementById("clique");
     const alturaAtual = minhaDiv.clientHeight;
     const novaAltura = alturaAtual + 50; // Aumenta 50 pixels (ajuste conforme necessário)
     var nome = document.getElementById('nome')
+    var sele = document.getElementById('selec')
     var nm = ''
     minhaDiv.style.borderRadius= 4+"%"
-    minhaDiv.style.height = novaAltura + "px";
+   
     minhaDiv.style.textAlign= 'center';
     minhaDiv.style.textAlign='center';
     //Adicionando Lista!
     itens.push(nome.value)
-    
-    minhaDiv.insertAdjacentHTML('beforeend',`<br>${c}-${itens[c]}`)
-    minhaDiv.insertAdjacentHTML('beforeend',`<div class='newdata'>${nome.value}</div>`)
-    c++
+    if (nome.value!='') {
+        minhaDiv.insertAdjacentHTML('beforeend',`<div class='newdata' id="${c}">${nome.value}<button onclick="retirar(${c})" style='background-color:red;'>Atendido</button></div><br>`)
+        minhaDiv.style.height =novaAltura + "px";
+    }
     nome.value=''
-
+    c++
+}
+function retirar(id){
+    const minhaDiv = document.getElementById("clique")
+    const alturaAtual = minhaDiv.clientHeight;;
+    const novaAltura = alturaAtual - 50
+    var retirado = document.getElementById(id)
+    retirado.style.background='blue'
+    retirado.remove(retirado);
+    minhaDiv.style.lineHeight='10px';
+    minhaDiv.style.textAlign= 'center';
+    minhaDiv.style.textAlign='center';
+    minhaDiv.style.height = novaAltura +"px";
+    
 }
---
+++ Matriz.js
@@ -0,0 +1,6 @@
+var clientes = [['maria','unha',123] ,//  00 01 02
+                ['carla','pe',563],//     10 11 12
+                ['sofia','unha',984] , // 20 21 22
+                ['ivani','pe',590]]//     30 31 32
+num = 3
+console.log(`O cliente é ${clientes[num][0]}, seu numero é ${clientes[num][2]} e quer fazer ${clientes[num][1]}`)
